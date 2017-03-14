0bfaa3d130120fe79b5640ae467714fae0669c83
Paul Brady <pbrady@hanleywood.com>
2017-03-14 15:40:21 -0700

    [Add] Node basic server

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+//set port variable to deployed port or 8000 for local host
+var port = process.env.PORT || 8001;
+
+//set dirname to client folder to serve static ASSETS (.css, .png, .jpg)
+app.use('/', express.static(__dirname + '/public'));
+
+
+app.listen(port, function() {
+  console.log("Listening on port " + port + "...");
+});
